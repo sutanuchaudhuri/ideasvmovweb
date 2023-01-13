a8aac18b3b81b4d3bfd077763e5a5ee1024fd009
sutanuchaudhuri <sutanu.chaudhuri@gmail.com>
2023-01-13 00:38:05 -0500

    default test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
     return (
         <div className="home">
-            <h1>Home</h1>
+            <h1>Home Tika test hello wih the entire world</h1>
         </div>
     );
 }
